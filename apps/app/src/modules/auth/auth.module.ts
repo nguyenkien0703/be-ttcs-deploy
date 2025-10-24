fa352896d4030b1042167a4cbed545c549cbd7ac
kiennv <kiennv.contact@gmail.com>
2025-10-24 16:07:32 +0700

    format

--- apps/app/src/modules/auth/auth.module.ts
+++ apps/app/src/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UserModule } from '../users/user.module'
-import {EmailModule} from "../emails/email.module";
+import { EmailModule } from '../emails/email.module'
 
 @Module({
     imports: [UserModule, EmailModule],
